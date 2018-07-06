3dfe5cd5c54ec5ed9502fcb2c296413a23bb7a82
fmanokhin <fmanokhin@gmail.com>
2018-07-06 11:38:40 +0300

    Added model Pop

---
+++ inventory/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Pop(models.Model):
+    title = models.CharField(max_length=200)
+    address = models.TextField()
+    contacts = models.TextField()
+    manager = models.TextField()
+    bandwidth = models.TextField()
+    dnsname = models.TextField()
+    comments = models.TextField()
+
+    def new_pop(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
